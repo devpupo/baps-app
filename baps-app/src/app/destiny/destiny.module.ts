a8843cb9139beb6babc4ba492fb05fe7456edaca
devpupo <devpupo@outlook.com>
2021-03-17 19:31:35 -0300

    criado componente page destiny

---
+++ baps-app/src/app/destiny/destiny.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { DestinyPage } from './destiny.page';
+
+import { DestinyPageRoutingModule } from './destiny-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    DestinyPageRoutingModule
+  ],
+  declarations: [DestinyPage]
+})
+export class HomePageModule {}
---
+++ baps-app/src/app/destiny/destiny.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destiny',
+  templateUrl: 'destiny.page.html',
+  styleUrls: ['destiny.page.scss'],
+})
+export class DestinyPage {
+
+  constructor() {}
+
+}
