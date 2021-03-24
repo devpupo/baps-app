3dc0aad7bc859013699c6b4a11700975a5a1d06a
pazevedo <pedro.b.azevedo@capgemini.com>
2021-03-24 19:53:15 -0300

    merged differents projects

--- baps-app/src/app/app-routing.module.ts
+++ baps-app/src/app/app-routing.module.ts
@@ -1,36 +1,44 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginPage } from './login/login.page';
 
 const routes: Routes = [
   {
     path: 'destiny',
-    loadChildren: () => import('./destiny/destiny.module').then( m => m.DestinyPageModule)
+    loadChildren: () => import('./destiny/destiny.module').then(m => m.DestinyPageModule)
   },
   {
     path: 'about',
-    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
+    loadChildren: () => import('./about/about.module').then(m => m.AboutPageModule)
   },
   {
     path: 'blog',
-    loadChildren: () => import('./blog/blog.module').then( m => m.BlogPageModule)
+    loadChildren: () => import('./blog/blog.module').then(m => m.BlogPageModule)
   },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: 'account',
-    loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
+    loadChildren: () => import('./account/account.module').then(m => m.AccountPageModule)
   },
   {
     path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+    loadChildren: () => import('./register/register.module').then(m => m.RegisterPageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'LoginPage', component: LoginPage
+  }
 ];
 
 @NgModule({
--- baps-app/src/app/home/home.page.ts
+++ baps-app/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonSlides } from '@ionic/angular';
+import { Promotion } from '../model/promotion.model';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  slideOpts : any;
 
-  constructor() {}
+  promotions: Promotion[] = [];
 
+  @ViewChild(IonSlides, {static: true}) slides: IonSlides;
+
+  constructor(private router: Router) { 
+    this.InitializePromotions();
+    this.slideOpts = {
+      initialSlide: 1,
+      slidesPerView: 1,
+      speed: 400,
+      autoplay: true
+    };
+  }
+
+  InitializePromotions() {
+
+    this.promotions.push(new Promotion('Paris', 6224, 10, 'GRU', 'CDG', './../../assets/imagens/paris.jpg'));
+    this.promotions.push(new Promotion('New York', 8284, 10, 'GRU', 'NYC', './../../assets/imagens/new-york.jpg'));
+    this.promotions.push(new Promotion('Toronto', 10082, 10, 'GRU', 'YVZ', './../../assets/imagens/toronto.jpg'));
+  }  
 }
