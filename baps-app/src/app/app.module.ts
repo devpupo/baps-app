65fedcb848a2a629f81977efc3d3f256163cb778
pazevedo <pedro.b.azevedo@capgemini.com>
2021-05-10 20:08:10 -0300

    Adding offiline storage

--- baps-app/src/app/app.module.ts
+++ baps-app/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -8,11 +8,19 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 //import { ToolBarComponent } from './components/toolbar/toolbar.component';
 
+import { IonicStorageModule } from '@ionic/storage'
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), IonicStorageModule.forRoot(), AppRoutingModule], providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule implements OnInit {
+
+  constructor(private storage: Storage) { }
+
+  async ngOnInit() {
+    await this.storage.create();
+  }
+}
