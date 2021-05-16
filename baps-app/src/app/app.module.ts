299e2fdc3a8ae5dec3d1a341e6fc636e0e98c64b
Guilherme <fontesguilherme9@gmail.com>
2021-05-16 13:56:51 -0300

    Correção Storage

--- baps-app/src/app/app.module.ts
+++ baps-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { NgModule, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { IonicStorageModule } from "@ionic/storage-angular";
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -8,14 +9,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 //import { ToolBarComponent } from './components/toolbar/toolbar.component';
 
-import { IonicStorageModule } from '@ionic/storage'
-
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), IonicStorageModule.forRoot(), AppRoutingModule], providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,IonicStorageModule.forRoot()],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule{}
-  
-
+export class AppModule {}
