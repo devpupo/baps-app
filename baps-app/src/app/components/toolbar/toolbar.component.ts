4d2af21c9c02be2cbb6364408444d05028cc1bb5
thwmaine <fernandacricci11@gmail.com>
2021-06-05 20:17:17 -0300

    Implementação da rota condicional

--- baps-app/src/app/components/toolbar/toolbar.component.ts
+++ baps-app/src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/model/user';
+import { LoginService } from 'src/app/services/loginService/login.service';
+import { RegisterService } from 'src/app/services/registerService/register-service.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolBarComponent implements OnInit {
 
-  constructor() {}
+  public id: number;
 
-  ngOnInit(){
+  public usuarios = this.user.allUsers();
+  currentUser: User;
 
+  constructor(private router: Router,
+    private user: RegisterService,
+    private loginService: LoginService) { }
+
+  async ngOnInit() {
+    this.currentUser = await this.loginService.getCurrentUser();
+    this.usuarios = [this.currentUser];
   }
+  onClick() {
+    let result;
 
+    if (this.currentUser == null) {
+      result = this.router.navigate(['/accountoff']);
+      console.log("teste");
+    }
+    result = this.router.navigate(['/account', this.currentUser.id]);
+  }
 }
